2dee662daaffc0a29dd79c50887af196cdd4fab6
Redstone256 <captain.replay314@8shield.net>
2024-06-23 19:25:07 +0530

    Refactored Code: Dedicated file for List and API endpoints
    - Using Solution Explorer, created a "Endpoints" Folder.
    - Contains "GamesEndpoints"
    - class contains "MapGamesEndpoints(this WebApplication app)" method
      with return type of WebApplication.
    - method is used in Program.cs

---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,100 @@
+﻿using GameStore.DTOs;
+
+namespace GameStore;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndPointName = "GetGame";
+
+    private static readonly List<GameDto> games = [
+        new (
+            1,
+            "Street Fighter",
+            "Fighting",
+            19.99M,
+            new DateOnly(1992, 7, 5)
+        ),
+        new (
+            2,
+            "Final Fantasy XIV",
+            "RolePlaying",
+            59.99M,
+            new DateOnly(2010, 9, 30)
+        ),
+        new (
+            3,
+            "FIFA 23",
+            "Sports",
+            69.99M,
+            new DateOnly(2022, 9, 27)
+        ),
+    ];
+
+    // return: WebApplication
+    // It can be chained by other WebApplication 
+    // which is done by calling method
+    public static WebApplication MapGamesEndpoints(this WebApplication app)
+    {
+        app.MapGet("/", () => "Hello World!");
+
+        // Get /games
+        app.MapGet("games", () => games);
+
+        // Get /games/:id
+        app.MapGet("games/{id}", (int id) =>
+        {
+            GameDto? game = games.Find(game => game.Id == id);
+            return game is null ? Results.NotFound()
+                                : Results.Ok(game);
+        })
+            .WithName(GetGameEndPointName);
+
+        // Post /games
+        app.MapPost("games", (CreateGameDto newGame) =>
+        {
+            GameDto game = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id });
+        });
+
+        // PUT /games
+        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+            // Invalid index
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+            games[index] = new GameDto(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+
+        });
+
+        // Delete /games/1
+
+        app.MapDelete("games/{id}", (int id) =>
+        {
+            games.RemoveAll(game => id == game.Id);
+            return Results.NoContent();
+        }
+        );
+        
+        return app;
+    }
+}
--- obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540e92253ebf8ae4e14375ae78da90dfec7fa6ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
