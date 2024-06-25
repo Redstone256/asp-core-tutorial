32b5b6a7087d295d5afb897d87b7470816144daa
Redstone256 <captain.replay314@8shield.net>
2024-06-25 21:38:31 +0530

    Fixed Error: Root cause was builder.Build() happening before builder.Services.AddSqlite
    - Fixed the error.
    - Migrations adding successfully but setup pending.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@ using GameStore.Endpoints;
 using GameStore.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
-
 builder.Services.AddSqlite<GameStoreContext>(connString);
+
+var app = builder.Build();
 
 app.MapGamesEndpoints();
 
--- obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74b6bf890fab0f89f5bf1a26874389fc91d7d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ded30d6c11706003982986d0a19f149295f25d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
